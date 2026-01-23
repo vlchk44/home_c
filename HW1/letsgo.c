bf213ef0e9ed6c259c4b9a837bbdcd58cd5ed51a
Eduard Rakhmatullin <net.d@mail.ru>
2026-01-23 15:44:33 +0300

    Добавил 1ДЗ

---
+++ HW1/letsgo.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  letsgo
+//
+//  Created by Эдуард Рахматуллин on 23.01.2026.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Let's\n");
+    printf("    go\n");
+    printf("        to walk\n");
+    return 0;
+}
