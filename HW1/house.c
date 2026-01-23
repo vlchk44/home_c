e8413bdbfaa0cba0fd92e0e63cd6f84ec697a79f
Eduard Rakhmatullin <net.d@mail.ru>
2026-01-23 15:28:51 +0300

    Убрал .DS_Store и добавил в gitignore

---
+++ HW1/house.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  house
+//
+//  Created by Эдуард Рахматуллин on 23.01.2026.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("     *\n");
+    printf("    ***\n");
+    printf("   *****\n");
+    printf("  *******\n");
+    printf("   HHOHH\n");
+    printf("   ZZZZZ\n");
+    return 0;
+}
